8a4bcf291678c4257d8837d6099b553b61b36a08
R. Richards <rdrrichards@hotmail.com>
2016-11-24 11:48:58 -0600

    A little file clean up

--- QIQO.Business.Contracts/EntityServices/IEntityAttributeEntityService.cs
+++
@@ -1,8 +0,0 @@
-﻿using QIQO.Business.Entities;
-using QIQO.Common.Contracts;
-using QIQO.Data.Entities;
-
-namespace QIQO.Business.Contracts
-{
-    //public interface IEntityAttributeEntityService : IEntityService<EntityAttribute, AttributeData> { }
-}
--- QIQO.Business.Contracts/EntityServices/IEntityProductEntityService.cs
+++
@@ -1,8 +0,0 @@
-﻿using QIQO.Business.Entities;
-using QIQO.Common.Contracts;
-using QIQO.Data.Entities;
-
-namespace QIQO.Business.Contracts
-{
-    //public interface IEntityProductEntityService : IEntityService<EntityProduct, EntityProductData> { }
-}
